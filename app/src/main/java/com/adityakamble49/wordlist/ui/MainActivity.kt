a388fc0be0ca2dd03ec58f33af7cdd8464f32081
Aditya Kamble <adityakamble49@gmail.com>
2018-04-05 11:44:30 +0530

    Kotlin gradle plugin added

--- app/src/main/java/com/adityakamble49/wordlist/ui/MainActivity.kt
+++ app/src/main/java/com/adityakamble49/wordlist/ui/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.adityakamble49.wordlist.ui
 
-import android.content.Context
 import android.os.Bundle
 import com.adityakamble49.wordlist.R
-import com.adityakamble49.wordlist.di.qualifier.ApplicationContext
 import com.adityakamble49.wordlist.ui.common.BaseInjectableActivity
 import com.adityakamble49.wordlist.ui.list.WordListFragment
 import com.adityakamble49.wordlist.utils.addFragment
-import timber.log.Timber
-import javax.inject.Inject
 
 class MainActivity : BaseInjectableActivity() {
 
