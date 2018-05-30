6ffced83ed1fe140b47bfddf1232b3b752e4066d
Aditya Kamble <adityakamble49@gmail.com>
2018-05-30 12:06:58 +0530

    DictateModeTypes added

--- app/src/main/java/com/adityakamble49/wordlist/utils/Constants.java
+++ app/src/main/java/com/adityakamble49/wordlist/utils/Constants.java
@@ -18,11 +18,19 @@ public class Constants {
         public static final String ENTRY_WORD_LIST_DEFAULT_2 = "Manhattan Advanced - Alphabetical";
     }
 
+    public enum DictateModeTypes {
+        WORD_COMPLETE_INFO,
+        WORD_DEFINITION,
+        WORD_ONLY
+    }
+
     public static class ReferenceUrls {
         public static final String PERSONAL_WEBSITE = "http://adityakamble49.com";
         public static final String GITHUB_PROFILE = "https://github.com/adityakamble49";
         public static final String TWITTER_PROFILE = "https://twitter.com/adityakamble49";
-        public static final String CREDIT_WORD_LIST = "https://github.com/maykulkarni/Manhattan-GRE-Words-Shuffled";
-        public static final String CREDIT_APP_ICON = "https://www.flaticon.com/free-icon/checklist_273504#term=list&page=1&position=96";
+        public static final String CREDIT_WORD_LIST = "https://github" +
+                ".com/maykulkarni/Manhattan-GRE-Words-Shuffled";
+        public static final String CREDIT_APP_ICON = "https://www.flaticon" +
+                ".com/free-icon/checklist_273504#term=list&page=1&position=96";
     }
 }
