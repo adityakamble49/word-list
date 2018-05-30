776c15c19a598c37e0a973431041e93a96a7bbb6
Aditya Kamble <adityakamble49@gmail.com>
2018-05-30 12:07:46 +0530

    Preference Helper - Dictate Mode Type Preference added

--- app/src/main/java/com/adityakamble49/wordlist/cache/PreferenceHelper.kt
+++ app/src/main/java/com/adityakamble49/wordlist/cache/PreferenceHelper.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.adityakamble49.wordlist.BuildConfig
 import com.adityakamble49.wordlist.di.qualifier.ApplicationContext
+import com.adityakamble49.wordlist.utils.Constants
 import javax.inject.Inject
 
 /**
@@ -16,16 +17,22 @@ class PreferenceHelper @Inject constructor(@ApplicationContext private val conte
 
     companion object {
         private const val PREF_WORDLIST_PACKAGE_NAME = BuildConfig.APPLICATION_ID
+        private const val PREF_WORDLIST_SETTINGS = "${BuildConfig.APPLICATION_ID}_preferences"
         private const val PREF_KEY_CURRENT_LIST_ID = "current_list_type"
         const val DEFAULT_CURRENT_LIST_ID = 1
         private const val PREF_KEY_WORDS_IMPORTED = "words_imported"
         const val DEFAULT_KEY_WORDS_IMPORTED = false
+        private const val PREF_KEY_DICTATE_MODE_TYPE = "dictate_mode_type"
+        val DEFAULT_DICTATE_MODE_TYPE = Constants.DictateModeTypes.WORD_COMPLETE_INFO.name.toLowerCase()
     }
 
     private val wordListPref: SharedPreferences
+    private val wordListSettings: SharedPreferences
 
     init {
         wordListPref = context.getSharedPreferences(PREF_WORDLIST_PACKAGE_NAME,
+                Context.MODE_PRIVATE)
+        wordListSettings = context.getSharedPreferences(PREF_WORDLIST_SETTINGS,
                 Context.MODE_PRIVATE)
     }
 
@@ -38,4 +45,7 @@ class PreferenceHelper @Inject constructor(@ApplicationContext private val conte
         get() = wordListPref.getBoolean(PREF_KEY_WORDS_IMPORTED, DEFAULT_KEY_WORDS_IMPORTED)
         set(areWordsImported) = wordListPref.edit().putBoolean(PREF_KEY_WORDS_IMPORTED,
                 areWordsImported).apply()
+
+    var dictateModeType: String = DEFAULT_DICTATE_MODE_TYPE
+        get() = wordListSettings.getString(PREF_KEY_DICTATE_MODE_TYPE, DEFAULT_DICTATE_MODE_TYPE)
 }
