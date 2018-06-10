fc491d069a3528bbcb8c4f9e716880ea5aaee725
Aditya Kamble <adityakamble49@gmail.com>
2018-06-10 10:06:02 +0530

    hasSpecialChar() Extension function added

--- app/src/main/java/com/adityakamble49/wordlist/utils/Extensions.kt
+++ app/src/main/java/com/adityakamble49/wordlist/utils/Extensions.kt
@@ -3,11 +3,24 @@ package com.adityakamble49.wordlist.utils
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import java.util.regex.Pattern
 
 /**
- * @author kamble_ad
+ * Common Extensions
+ *
+ * @author Aditya Kamble
  * @since 4/4/2018
  */
+
+
+val specialCharPattern: Pattern = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE)
+
+
 fun getGson(): Gson = GsonBuilder()
         .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
         .create()
+
+fun String.hasSpecialChar(): Boolean {
+    val matcher = specialCharPattern.matcher(this)
+    return matcher.find()
+}
