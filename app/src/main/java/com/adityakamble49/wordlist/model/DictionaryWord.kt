f3b2aed9616f8d6769f788acbb9df3248a30d593
Aditya Kamble <adityakamble49@gmail.com>
2018-06-14 15:54:13 +0530

    Dictionary Word Model added

---
+++ app/src/main/java/com/adityakamble49/wordlist/model/DictionaryWord.kt
@@ -0,0 +1,13 @@
+package com.adityakamble49.wordlist.model
+
+/**
+ * Word Dictionary
+ *
+ * @author Aditya Kamble
+ * @since 14/6/2018
+ */
+data class DictionaryWord(
+        var type: String?,
+        var definition: String?,
+        var example: String?
+)
