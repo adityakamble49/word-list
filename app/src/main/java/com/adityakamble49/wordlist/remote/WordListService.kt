3f3efda89bbb9bb9e709f739298e02224c31fea4
Aditya Kamble <adityakamble49@gmail.com>
2018-06-14 15:55:45 +0530

    Get Dictionary Word Info REST Service added

--- app/src/main/java/com/adityakamble49/wordlist/remote/WordListService.kt
+++ app/src/main/java/com/adityakamble49/wordlist/remote/WordListService.kt
@@ -1,5 +1,10 @@
 package com.adityakamble49.wordlist.remote
 
+import com.adityakamble49.wordlist.model.DictionaryWord
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 /**
  * Word List service
  *
@@ -7,5 +12,8 @@ package com.adityakamble49.wordlist.remote
  * @since 12/6/2018
  */
 interface WordListService {
+
+    @GET("dictionary/{word}")
+    fun getWordInformation(@Path("word") word: String): Observable<List<DictionaryWord>>
 
 }
