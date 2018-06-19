d2fd40e5bd2cb8f65af040eef34ef819840affa0
Aditya Kamble <adityakamble49@gmail.com>
2018-06-19 22:35:57 +0530

    Word Activity Single Mode Test added

---
+++ app/src/test/java/com/adityakamble49/wordlist/ui/word/WordActivitySingleModeTest.kt
@@ -0,0 +1,44 @@
+package com.adityakamble49.wordlist.ui.word
+
+import android.content.Intent
+import com.adityakamble49.wordlist.BuildConfig
+import org.junit.Assert
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+/**
+ * Word Test
+ *
+ * @author Aditya Kamble
+ * @since 19/6/2018
+ */
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class)
+class WordActivitySingleModeTest {
+
+    private lateinit var singleWordActivity: WordActivity
+
+    @Before
+    fun setUp() {
+        val singleWordIntent = Intent(Intent.ACTION_VIEW)
+        singleWordIntent.putExtra(WordActivity.IE_KEY_WORD_ACTIVITY_MODE,
+                WordActivity.Companion.WordActivityMode.SINGLE)
+        singleWordActivity = Robolectric.buildActivity(WordActivity::class.java,
+                singleWordIntent).create().get()
+    }
+
+    @Test
+    fun shouldNotBeNull() {
+        assertNotNull(singleWordActivity)
+    }
+
+    @Test
+    fun shouldHaveWordListAsTitle() {
+        Assert.assertEquals("Word List", singleWordActivity.supportActionBar?.title)
+    }
+}
