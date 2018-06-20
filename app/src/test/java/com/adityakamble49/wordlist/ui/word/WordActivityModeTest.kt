ac746c21f69a75981d45887813e28ed335888568
Aditya Kamble <adityakamble49@gmail.com>
2018-06-20 17:07:01 +0530

    Word Activity - Edit Mode test added

--- app/src/test/java/com/adityakamble49/wordlist/ui/word/WordActivityModeTest.kt
+++ app/src/test/java/com/adityakamble49/wordlist/ui/word/WordActivityModeTest.kt
@@ -2,6 +2,7 @@ package com.adityakamble49.wordlist.ui.word
 
 import android.content.Intent
 import com.adityakamble49.wordlist.BuildConfig
+import com.adityakamble49.wordlist.R
 import kotlinx.android.synthetic.main.activity_word_info.*
 import org.junit.Assert
 import org.junit.Assert.assertEquals
@@ -26,6 +27,7 @@ import org.robolectric.annotation.Config
 abstract class WordActivityModeTest {
 
     protected lateinit var wordActivityController: ActivityController<WordActivity>
+    protected lateinit var wordActivity: WordActivity
 
     @Before
     fun setUp() {
@@ -33,6 +35,7 @@ abstract class WordActivityModeTest {
         singleWordIntent.putExtra(WordActivity.IE_KEY_WORD_ACTIVITY_MODE, getActivityMode().ordinal)
         wordActivityController = Robolectric.buildActivity(WordActivity::class.java,
                 singleWordIntent).create().visible()
+        wordActivity = wordActivityController.get()
     }
 
     abstract fun getActivityMode(): WordActivity.Companion.WordActivityMode
@@ -54,7 +57,7 @@ abstract class WordActivityModeTest {
     fun checkOptionMenuAvailable() {
         val availableOptionMenus = getAvailableOptionMenus()
         availableOptionMenus.forEach {
-            val menuItem = shadowOf(wordActivityController.get()).optionsMenu.findItem(it)
+            val menuItem = shadowOf(wordActivity).optionsMenu.findItem(it)
             assertEquals(true, menuItem.isVisible)
         }
     }
@@ -63,9 +66,8 @@ abstract class WordActivityModeTest {
 
     @Test
     fun checkBottomNavigationController() {
-        assertEquals(getControllerVisibility(), wordActivityController.get().fab_dictate.visibility)
-        assertEquals(getControllerVisibility(),
-                wordActivityController.get().llBottomNavigation.visibility)
+        assertEquals(getControllerVisibility(), wordActivity.fab_dictate.visibility)
+        assertEquals(getControllerVisibility(), wordActivity.llBottomNavigation.visibility)
     }
 
     abstract fun getControllerVisibility(): Int
@@ -73,10 +75,42 @@ abstract class WordActivityModeTest {
     @Test
     fun checkWordInfoEmpty() {
         if (shouldWordInfoEmpty()) {
-            assertEquals("", wordActivityController.get().word_information.text.toString())
-            assertEquals("", wordActivityController.get().word_mnemonic.text.toString())
+            assertEquals("", wordActivity.word_information.text.toString())
+            assertEquals("", wordActivity.word_mnemonic.text.toString())
         }
     }
 
     abstract fun shouldWordInfoEmpty(): Boolean
+
+    @Test
+    fun checkEditMode() {
+        val availableOptionMenus = getAvailableOptionMenus()
+        if (availableOptionMenus.contains(R.id.action_edit_word)) {
+
+            // Prepare Edit and Submit Menu
+            val editMenuItem = shadowOf(wordActivity).optionsMenu.findItem(R.id.action_edit_word)
+            val submitMenuItem = shadowOf(wordActivity).optionsMenu.findItem(
+                    R.id.action_submit_word)
+
+            // Check if Edit and Submit Menu Visibility
+            assertEquals(true, editMenuItem.isVisible)
+            assertEquals(false, submitMenuItem.isVisible)
+
+            // Perform On Click Edit Menu Item
+            wordActivityController.get().onOptionsItemSelected(editMenuItem)
+
+            // Check if Word Info is editable
+            assertEquals(true, wordActivity.word_name.isEnabled)
+            assertEquals(true, wordActivity.word_type.isEnabled)
+            assertEquals(true, wordActivity.word_pronunciation.isEnabled)
+            assertEquals(true, wordActivity.word_information.isEnabled)
+            assertEquals(true, wordActivity.word_mnemonic.isEnabled)
+            assertEquals(true, wordActivity.word_information.isCursorVisible)
+            assertEquals(true, wordActivity.word_mnemonic.isCursorVisible)
+
+            // Check if Edit and Submit Menu Visibility
+            assertEquals(false, editMenuItem.isVisible)
+            assertEquals(true, submitMenuItem.isVisible)
+        }
+    }
 }
