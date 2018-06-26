9782d894de0b5a0ec02cef4d501e87eb5517b9ed
Aditya Kamble <adityakamble49@gmail.com>
2018-06-27 00:20:32 +0530

    Marketplace Word List status icon updated

--- app/src/main/java/com/adityakamble49/wordlist/ui/marketplace/MarketplaceListAdapter.kt
+++ app/src/main/java/com/adityakamble49/wordlist/ui/marketplace/MarketplaceListAdapter.kt
@@ -1,8 +1,12 @@
 package com.adityakamble49.wordlist.ui.marketplace
 
+import android.graphics.drawable.Drawable
+import android.support.v4.content.res.ResourcesCompat
 import android.view.View
+import android.widget.ImageView
 import com.adityakamble49.wordlist.R
 import com.adityakamble49.wordlist.model.MarketplaceWordList
+import com.adityakamble49.wordlist.model.WordListStatus
 import com.adityakamble49.wordlist.ui.common.SimpleListAdapter
 import com.adityakamble49.wordlist.utils.getWordListNameFromFileName
 import kotlinx.android.synthetic.main.marketplace_list_item.view.*
@@ -30,6 +34,8 @@ open class MarketplaceListAdapter : SimpleListAdapter<MarketplaceWordList>() {
             with(itemView) {
                 item_title.text = getWordListNameFromFileName(item.name)
                 item_download_image.setOnClickListener(this@ViewHolder)
+                item_download_image.setImageDrawable(
+                        getDrawableByStatus(item_download_image, item.status))
             }
         }
 
@@ -40,5 +46,19 @@ open class MarketplaceListAdapter : SimpleListAdapter<MarketplaceWordList>() {
             }
         }
 
+    }
+
+    private fun getDrawableByStatus(item_download_image: ImageView, status: String): Drawable? {
+        val context = item_download_image.context
+        return when (status) {
+            WordListStatus.NOT_AVAILABLE.name -> ResourcesCompat.getDrawable(context.resources,
+                    R.drawable.ic_download, context.theme)
+            WordListStatus.AVAILABLE.name -> ResourcesCompat.getDrawable(context.resources,
+                    R.drawable.ic_available, context.theme)
+            WordListStatus.UPDATE_AVAILABLE.name -> ResourcesCompat.getDrawable(context.resources,
+                    R.drawable.ic_update, context.theme)
+            else -> ResourcesCompat.getDrawable(context.resources, R.drawable.ic_download,
+                    context.theme)
+        }
     }
 }
