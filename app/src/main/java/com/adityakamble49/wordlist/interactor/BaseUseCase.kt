460050c124f3cd19d47bc5bdcc9b8642fd794733
Aditya Kamble <adityakamble49@gmail.com>
2018-06-28 11:02:58 +0530

    Base Rx Use case added with Composite Disposables

---
+++ app/src/main/java/com/adityakamble49/wordlist/interactor/BaseUseCase.kt
@@ -0,0 +1,25 @@
+package com.adityakamble49.wordlist.interactor
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+/**
+ * Base Use Case
+ *
+ * @author Aditya Kamble
+ * @since 28/6/2018
+ */
+abstract class BaseUseCase {
+
+    private val disposables = CompositeDisposable()
+
+    protected fun addDisposables(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+
+    fun dispose() {
+        if (!disposables.isDisposed) {
+            disposables.dispose()
+        }
+    }
+}
