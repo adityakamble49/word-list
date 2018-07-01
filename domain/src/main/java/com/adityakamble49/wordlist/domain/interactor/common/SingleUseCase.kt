a375df66d2cb393891b525b28578907ebf0f37fb
Aditya Kamble <adityakamble49@gmail.com>
2018-07-01 23:33:34 +0530

    Single Observable Use case added

---
+++ domain/src/main/java/com/adityakamble49/wordlist/domain/interactor/common/SingleUseCase.kt
@@ -0,0 +1,43 @@
+package com.adityakamble49.wordlist.domain.interactor.common
+
+import com.adityakamble49.wordlist.domain.executor.PostExecutionThread
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+
+/**
+ * Single Use Case
+ *
+ * @author Aditya Kamble
+ * @since 1/7/2018
+ */
+abstract class SingleUseCase<T, in Param> @Inject constructor(
+        private val postExecutionThread: PostExecutionThread) {
+
+    private var disposables = CompositeDisposable()
+
+    protected abstract fun buildUseCase(param: Param? = null): Single<T>
+
+    fun execute(observer: DisposableSingleObserver<T>, param: Param? = null) {
+        val observable = buildUseCase(param)
+                .subscribeOn(Schedulers.io())
+                .observeOn(postExecutionThread.scheduler)
+        addDisposables(observable.subscribeWith(observer))
+    }
+
+    private fun addDisposables(disposable: Disposable) {
+        if (disposables.isDisposed) {
+            disposables = CompositeDisposable()
+        }
+        disposables.add(disposable)
+    }
+
+    fun dispose() {
+        if (!disposables.isDisposed) {
+            disposables.dispose()
+        }
+    }
+}
