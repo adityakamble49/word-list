31f6183cb29ea181c36b5152f2393b8c0c535612
Aditya Kamble <adityakamble49@gmail.com>
2018-07-02 12:49:04 +0530

    Word List Mapper Test added

---
+++ data/src/test/java/com/adityakamble49/wordlist/data/mapper/WordListMapperTest.kt
@@ -0,0 +1,27 @@
+package com.adityakamble49.wordlist.data.mapper
+
+import com.adityakamble49.wordlist.data.model.WordListEntity
+import com.adityakamble49.wordlist.data.test.WordListDataFactory
+import com.adityakamble49.wordlist.domain.model.WordList
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class WordListMapperTest {
+
+    private val mapper = WordListMapper()
+
+    @Test
+    fun mapFromEntityTest() {
+        val entity = WordListDataFactory.makeWordListEntity()
+        val model = mapper.mapFromEntity(entity)
+        assertDataEquals(entity, model)
+    }
+
+    private fun assertDataEquals(entity: WordListEntity, model: WordList) {
+        assertEquals(entity.id, model.id)
+        assertEquals(entity.hash, model.hash)
+        assertEquals(entity.marketplaceFilename, model.marketplaceFilename)
+        assertEquals(entity.name, model.name)
+        assertEquals(entity.lastWordId, model.lastWordId)
+    }
+}
