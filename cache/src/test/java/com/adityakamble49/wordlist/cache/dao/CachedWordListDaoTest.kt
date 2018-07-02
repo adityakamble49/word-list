7454464852cc8fc59fd4b714603859fbad67b925
Aditya Kamble <adityakamble49@gmail.com>
2018-07-02 21:36:15 +0530

    Cached Word List Dao - Get Word List Test added

--- cache/src/test/java/com/adityakamble49/wordlist/cache/dao/CachedWordListDaoTest.kt
+++ cache/src/test/java/com/adityakamble49/wordlist/cache/dao/CachedWordListDaoTest.kt
@@ -1,11 +1,13 @@
 package com.adityakamble49.wordlist.cache.dao
 
+import android.arch.core.executor.testing.InstantTaskExecutorRule
 import android.arch.persistence.room.Room
 import android.database.sqlite.SQLiteConstraintException
 import com.adityakamble49.wordlist.cache.db.WordListDatabase
 import com.adityakamble49.wordlist.cache.test.WordListDataFactory
 import junit.framework.TestCase.assertNotNull
 import org.junit.After
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -13,6 +15,10 @@ import org.robolectric.RuntimeEnvironment
 
 @RunWith(RobolectricTestRunner::class)
 class CachedWordListDaoTest {
+
+    @Rule
+    @JvmField
+    var instantTaskExecutableRule = InstantTaskExecutorRule()
 
     private val database = Room.inMemoryDatabaseBuilder(
             RuntimeEnvironment.application.applicationContext, WordListDatabase::class.java)
@@ -36,5 +42,13 @@ class CachedWordListDaoTest {
         val cachedWordList = WordListDataFactory.makeCachedWordList()
         database.cachedWordListDao().insert(cachedWordList)
         database.cachedWordListDao().insert(cachedWordList)
+    }
+
+    @Test
+    fun getWordListTest() {
+        val cachedWordList = WordListDataFactory.makeCachedWordList()
+        database.cachedWordListDao().insert(cachedWordList)
+        val testObserver = database.cachedWordListDao().getWordList().test()
+        testObserver.assertValue(listOf(cachedWordList))
     }
 }
