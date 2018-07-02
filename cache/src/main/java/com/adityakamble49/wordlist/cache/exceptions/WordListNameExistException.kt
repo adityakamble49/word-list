689186247eaeb0bbe97c0a3474bff5643df8c6f0
Aditya Kamble <adityakamble49@gmail.com>
2018-07-02 19:50:28 +0530

    WordList Name Exist exception moved to Cache module

--- cache/src/main/java/com/adityakamble49/wordlist/cache/exceptions/WordListNameExistException.kt
+++ cache/src/main/java/com/adityakamble49/wordlist/cache/exceptions/WordListNameExistException.kt
@@ -1,9 +1,9 @@
-package com.adityakamble49.wordlist.domain.exceptions
+package com.adityakamble49.wordlist.cache.exceptions
 
 /**
  * Word List Name Exist Exception
  *
  * @author Aditya Kamble
- * @since 1/7/2018
+ * @since 2/7/2018
  */
 class WordListNameExistException constructor(override val message: String) : Exception()
