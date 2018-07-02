763c09efa5be280a9ddd8f1e335b2d970319c5ec
Aditya Kamble <adityakamble49@gmail.com>
2018-07-02 12:42:09 +0530

    Word List Repository implementation added

---
+++ data/src/main/java/com/adityakamble49/wordlist/data/WordListDataRepository.kt
@@ -0,0 +1,23 @@
+package com.adityakamble49.wordlist.data
+
+import com.adityakamble49.wordlist.data.mapper.WordListMapper
+import com.adityakamble49.wordlist.data.store.WordListDataStoreFactory
+import com.adityakamble49.wordlist.domain.model.WordList
+import com.adityakamble49.wordlist.domain.repository.WordListRepository
+import io.reactivex.Single
+import javax.inject.Inject
+
+class WordListDataRepository @Inject constructor(
+        private val mapper: WordListMapper,
+        private val factory: WordListDataStoreFactory) : WordListRepository {
+
+    override fun createWordList(wordList: WordList): Single<WordList> {
+        return factory.getDataStore()
+                .saveWordList(mapper.mapToEntity(wordList))
+                .map { wordListId ->
+                    WordList(wordListId.toInt(), wordList.hash,
+                            wordList.marketplaceFilename, wordList.name,
+                            wordList.lastWordId)
+                }
+    }
+}
