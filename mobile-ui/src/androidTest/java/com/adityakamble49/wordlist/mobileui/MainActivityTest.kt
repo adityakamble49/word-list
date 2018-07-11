09f8d9df95996b2aff12bb839fe08e94b5f48731
Aditya Kamble <adityakamble49@gmail.com>
2018-07-11 21:52:42 +0530

    MainActivityTest - createWordList and Displays result test added

--- mobile-ui/src/androidTest/java/com/adityakamble49/wordlist/mobileui/MainActivityTest.kt
+++ mobile-ui/src/androidTest/java/com/adityakamble49/wordlist/mobileui/MainActivityTest.kt
@@ -1,6 +1,7 @@
 package com.adityakamble49.wordlist.mobileui
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.contrib.RecyclerViewActions
 import android.support.test.espresso.matcher.ViewMatchers.*
@@ -9,8 +10,10 @@ import android.support.test.runner.AndroidJUnit4
 import com.adityakamble49.wordlist.domain.model.WordList
 import com.adityakamble49.wordlist.mobileui.test.TestWordListApp
 import com.adityakamble49.wordlist.mobileui.test.WordListDataFactory
+import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Flowable
+import io.reactivex.Single
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -47,8 +50,26 @@ class MainActivityTest {
         }
     }
 
+    @Test
+    fun createWordListDisplays() {
+        val wordLists = WordListDataFactory.makeWordLists(0)
+        stubWordListRepositoryGetWordList(Flowable.just(wordLists))
+        val wordList = WordListDataFactory.makeWordList()
+        stubWordListRepositoryCreateWordList(Single.just(wordList))
+        activity.launchActivity(null)
+        onView(withId(R.id.action_add)).perform(click())
+        onView(withId(R.id.rv_wordlist)).perform(
+                RecyclerViewActions.scrollToPosition<WordListAdapter.ViewHolder>(0))
+        onView(withId(R.id.rv_wordlist)).check(matches(hasDescendant(withText(wordList.name))))
+    }
+
     private fun stubWordListRepositoryGetWordList(flowable: Flowable<List<WordList>>) {
         whenever(TestWordListApp.appComponent().wordListRepository().getWordLists()).thenReturn(
                 flowable)
+    }
+
+    private fun stubWordListRepositoryCreateWordList(single: Single<WordList>) {
+        whenever(TestWordListApp.appComponent().wordListRepository().createWordList(
+                any())).thenReturn(single)
     }
 }
