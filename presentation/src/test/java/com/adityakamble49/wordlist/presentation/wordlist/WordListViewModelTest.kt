3fe8e72a3932c78fb90e51fd8c759180bec4458d
Aditya Kamble <adityakamble49@gmail.com>
2018-07-12 10:59:12 +0530

    Word List View Model - Test fixed

--- presentation/src/test/java/com/adityakamble49/wordlist/presentation/wordlist/WordListViewModelTest.kt
+++ presentation/src/test/java/com/adityakamble49/wordlist/presentation/wordlist/WordListViewModelTest.kt
@@ -2,7 +2,6 @@ package com.adityakamble49.wordlist.presentation.wordlist
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
 import com.adityakamble49.wordlist.domain.interactor.CreateWordList
-import com.adityakamble49.wordlist.domain.interactor.GetWordLists
 import com.adityakamble49.wordlist.presentation.mapper.WordListMapper
 import junit.framework.TestCase.assertTrue
 import org.junit.Before
@@ -19,7 +18,6 @@ class WordListViewModelTest {
     @get:Rule var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Mock private lateinit var createWordList: CreateWordList
-    @Mock private lateinit var getWordLists: GetWordLists
     @Mock private lateinit var mapper: WordListMapper
     private lateinit var wordlistViewModel: WordListViewModel
 
