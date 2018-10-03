c59bf1de016304b30a0eb59d01b3496eb7e8cae8
Aditya Kamble <adityakamble49@gmail.com>
2018-10-03 10:57:19 +0530

    Deleted old source code

--- app/src/main/java/com/adityakamble49/wordlist/interactor/WordListNameExistException.kt
+++
@@ -1,9 +0,0 @@
-package com.adityakamble49.wordlist.interactor
-
-/**
- * Word List Name Exist Exception
- *
- * @author Aditya Kamble
- * @since 10/6/2018
- */
-class WordListNameExistException constructor(override val message: String) : Exception()
