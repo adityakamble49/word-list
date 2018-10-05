86a3c6060ff5b4c763b251badd586e28f8249fa3
Aditya Kamble <adityakamble49@gmail.com>
2018-10-05 11:33:27 +0530

    Related Words Staggered Horizontal list added

--- app/src/main/java/com/adityakamble49/wordlist/ui/related/MeansLikeFragment.kt
+++ app/src/main/java/com/adityakamble49/wordlist/ui/related/MeansLikeFragment.kt
@@ -2,10 +2,14 @@ package com.adityakamble49.wordlist.ui.related
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.adityakamble49.wordlist.R
+import com.adityakamble49.wordlist.cache.entities.Word
+import kotlinx.android.synthetic.main.fragment_means_like.view.*
+import java.util.*
 
 /**
  * Means Like Fragment
@@ -15,13 +19,17 @@ import com.adityakamble49.wordlist.R
  */
 class MeansLikeFragment : Fragment() {
 
+    private lateinit var relatedWordsAdapter: RelatedWordsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_means_like, container, false)
+        val view = inflater.inflate(R.layout.fragment_means_like, container, false)
+        bindView(view)
+        return view
     }
 
     /*
@@ -30,5 +38,34 @@ class MeansLikeFragment : Fragment() {
 
     companion object {
         fun newInstance() = MeansLikeFragment()
+    }
+
+    private fun bindView(view: View) {
+        with(view) {
+            relatedWordsAdapter = RelatedWordsAdapter()
+            relatedWordsAdapter.listOfWord = getWords(100)
+            val staggeredGridLayoutManager = StaggeredGridLayoutManager(8,
+                    StaggeredGridLayoutManager.HORIZONTAL)
+            rv_means_like_words.adapter = relatedWordsAdapter
+            rv_means_like_words.layoutManager = staggeredGridLayoutManager
+        }
+    }
+
+    private fun getWords(count: Int): MutableList<Word> {
+        val random = Random()
+        val list = mutableListOf<Word>()
+        repeat(count) {
+            list.add(Word(0, getWordString(random, random.nextInt(10) + 5), "", arrayListOf()))
+        }
+        return list
+    }
+
+    private fun getWordString(random: Random, length: Int): String {
+        val input = "abcdefghijklmnopqrstuvwxyz"
+        val sb = StringBuilder()
+        repeat(length) {
+            sb.append(input[random.nextInt(26)])
+        }
+        return sb.toString()
     }
 }
---
+++ app/src/main/java/com/adityakamble49/wordlist/ui/related/RelatedWordsAdapter.kt
@@ -0,0 +1,41 @@
+package com.adityakamble49.wordlist.ui.related
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.adityakamble49.wordlist.R
+import com.adityakamble49.wordlist.cache.entities.Word
+import com.adityakamble49.wordlist.utils.inflate
+import kotlinx.android.synthetic.main.item_related_word.view.*
+
+/**
+ * Related Words Adapter
+ *
+ * @author Aditya Kamble
+ * @since 5/10/2018
+ */
+class RelatedWordsAdapter : RecyclerView.Adapter<RelatedWordsAdapter.ViewHolder>() {
+
+    var listOfWord = mutableListOf<Word>()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = parent.inflate(R.layout.item_related_word) as View
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listOfWord.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(position,
+            listOfWord[position])
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(position: Int, item: Word) {
+            with(itemView) {
+                tv_related_word.text = item.name
+            }
+        }
+    }
+}
