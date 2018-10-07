8819c0ef72d648779f0c55a036021ad0129093f6
Aditya Kamble <adityakamble49@gmail.com>
2018-10-07 11:56:14 +0530

    Word Activity - Show Title on Collapse only

--- app/src/main/java/com/adityakamble49/wordlist/ui/word/WordActivity.kt
+++ app/src/main/java/com/adityakamble49/wordlist/ui/word/WordActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import com.adityakamble49.wordlist.R
 import com.adityakamble49.wordlist.ui.common.BaseInjectableActivity
+import kotlinx.android.synthetic.main.activity_word.*
 import javax.inject.Inject
 
 /**
@@ -20,6 +21,8 @@ class WordActivity : BaseInjectableActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word)
+
+        bindView()
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -27,5 +30,27 @@ class WordActivity : BaseInjectableActivity() {
             android.R.id.home -> supportFinishAfterTransition()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun bindView() {
+        showTitleOnCollapseOnly()
+    }
+
+    private fun showTitleOnCollapseOnly() {
+        collapsing_toolbar.title = resources.getString(R.string.word_name_default)
+        var isShow = true
+        var scrollRange = -1
+        app_bar.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            if (scrollRange == -1) {
+                scrollRange = appBarLayout.totalScrollRange
+            }
+            if (scrollRange + verticalOffset == 0) {
+                collapsing_toolbar.title = resources.getString(R.string.word_name_default)
+                isShow = true
+            } else if (isShow) {
+                collapsing_toolbar.title = ""
+                isShow = false
+            }
+        }
     }
 }
