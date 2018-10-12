658bc9c8160a0973d64252316bc88bc2157fe2ad
Aditya Kamble <adityakamble49@gmail.com>
2018-10-12 23:17:14 +0530

    Edit and Delete option added to Word List

--- app/src/main/java/com/adityakamble49/wordlist/ui/wordlist/WordListActivity.kt
+++ app/src/main/java/com/adityakamble49/wordlist/ui/wordlist/WordListActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.adityakamble49.wordlist.R
+import com.afollestad.materialdialogs.MaterialDialog
 import kotlinx.android.synthetic.main.activity_word_list.*
 import kotlinx.android.synthetic.main.layout_wordlist_body.*
 
@@ -16,13 +20,28 @@ import kotlinx.android.synthetic.main.layout_wordlist_body.*
  */
 class WordListActivity : AppCompatActivity() {
 
+    // UI Fields
     private lateinit var wordListWordAdapter: WordListWordAdapter
+    private var deleteWordListConfirmDialog: MaterialDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_list)
 
         bindView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.word_list_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_word_list_edit -> editWordList()
+            R.id.action_word_list_delete -> deleteWordList()
+        }
+        return true
     }
 
     private fun bindView() {
@@ -64,5 +83,25 @@ class WordListActivity : AppCompatActivity() {
         rv_word_list_words.adapter = wordListWordAdapter
         rv_word_list_words.layoutManager = linearLayoutManager
         rv_word_list_words.addItemDecoration(dividerItemDecorator)
+    }
+
+    private fun editWordList() {}
+
+
+    private fun deleteWordList() {
+        if (deleteWordListConfirmDialog == null) {
+            deleteWordListConfirmDialog = MaterialDialog.Builder(this)
+                    .title(getString(R.string.delete_word_list_title))
+                    .content(getString(R.string.delete_word_list_content))
+                    .positiveText(getString(R.string.delete))
+                    .negativeText(getString(R.string.keep))
+                    .onPositive { _, _ ->
+                        Toast.makeText(this, getString(R.string.delete_word_list_success),
+                                Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                    .build()
+        }
+        deleteWordListConfirmDialog?.show()
     }
 }
