04fbf1534162fa1e78fdbec85e3b821771f40c11
Aditya Kamble <adityakamble49@gmail.com>
2018-10-12 23:55:38 +0530

    About Us Activity added | Action added in Settings

---
+++ app/src/main/java/com/adityakamble49/wordlist/ui/about/AboutUsActivity.kt
@@ -0,0 +1,17 @@
+package com.adityakamble49.wordlist.ui.about
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * About Us Activity
+ *
+ * @author Aditya Kamble
+ * @since 12/10/2018
+ */
+class AboutUsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
