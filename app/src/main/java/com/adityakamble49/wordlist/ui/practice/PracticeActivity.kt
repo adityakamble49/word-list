1ff43935dbe93cfa8a405cd94f757033a4a82605
Aditya Kamble <adityakamble49@gmail.com>
2018-10-12 12:00:25 +0530

    Practice - App Shortcut added

---
+++ app/src/main/java/com/adityakamble49/wordlist/ui/practice/PracticeActivity.kt
@@ -0,0 +1,18 @@
+package com.adityakamble49.wordlist.ui.practice
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Practice Activity
+ *
+ * @author Aditya Kamble
+ * @since 12/10/2018
+ */
+class PracticeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
